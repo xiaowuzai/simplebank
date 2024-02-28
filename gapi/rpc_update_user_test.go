6bb49667e4b2c25365ef5b2bb38daee2e7c417a4
zhanglaiyu <18345070876@163.com>
2024-02-28 19:06:01 +0800

    feature grpc-api-auth-test: 对gRPC需要认证的接口添加单元测试

---
+++ gapi/rpc_update_user_test.go
@@ -0,0 +1,145 @@
+package gapi
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+	mockdb "github.com/xiaowuzai/simplebank/db/mock"
+	db "github.com/xiaowuzai/simplebank/db/sqlc"
+	"github.com/xiaowuzai/simplebank/pb"
+	"github.com/xiaowuzai/simplebank/token"
+	"github.com/xiaowuzai/simplebank/util"
+	"go.uber.org/mock/gomock"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/status"
+)
+
+func newContextWithBearerToken(t *testing.T, tokenMaker token.Maker, username string, timeDuration time.Duration) context.Context {
+	token, _, err := tokenMaker.CreateToken(username, time.Minute)
+	bearToken := fmt.Sprintf("%s %s", authorizationBearer, token)
+	require.NoError(t, err)
+
+	md := metadata.MD{
+		authorizationHeader: []string{bearToken},
+	}
+	return metadata.NewIncomingContext(context.Background(), md)
+}
+
+func TestUpdateUserAPI(t *testing.T) {
+	user, _ := randomUser(t)
+
+	newFullName := util.RandomOwner()
+	newEmail := util.RandomEmail()
+	newUsername := util.RandomOwner()
+
+	cases := []struct {
+		name          string // test name
+		req           *pb.UpdateUserRequest
+		buildStubs    func(store *mockdb.MockStore)
+		buildContext  func(t *testing.T, tokenMaker token.Maker) context.Context
+		checkResponse func(t *testing.T, res *pb.UpdateUserResponse, err error)
+	}{
+		{
+			name: "OK",
+			req: &pb.UpdateUserRequest{
+				Username: user.Username,
+				FullName: &newFullName,
+				Email:    &newEmail,
+			},
+			buildStubs: func(store *mockdb.MockStore) {
+				arg := db.UpdateUserParams{
+					Username: user.Username,
+					FullName: sql.NullString{
+						String: newFullName,
+						Valid:  true,
+					},
+					Email: sql.NullString{
+						String: newEmail,
+						Valid:  true,
+					},
+				}
+				updatedUser := db.User{
+					Username:          user.Username,
+					HashedPassword:    user.HashedPassword,
+					FullName:          newFullName,
+					Email:             newEmail,
+					PasswordChangedAt: user.PasswordChangedAt,
+					CreatedAt:         user.CreatedAt,
+					IsEmailVerified:   user.IsEmailVerified,
+				}
+				store.EXPECT().
+					UpdateUser(gomock.Any(), gomock.Eq(arg)).
+					Times(1).
+					Return(updatedUser, nil)
+			},
+			buildContext: func(t *testing.T, tokenMaker token.Maker) context.Context {
+				return newContextWithBearerToken(t, tokenMaker, user.Username, time.Minute)
+			},
+			checkResponse: func(t *testing.T, res *pb.UpdateUserResponse, err error) {
+				require.NoError(t, err)
+				require.NotNil(t, res)
+				updatedUser := res.GetUser()
+				require.Equal(t, user.Username, updatedUser.Username)
+				require.Equal(t, newFullName, updatedUser.FullName)
+				require.Equal(t, newEmail, updatedUser.Email)
+			},
+		},
+		{
+			name: "UserNotFound",
+			req: &pb.UpdateUserRequest{
+				Username: newUsername, // 使用一个不存在的username 更新数据
+				FullName: &newFullName,
+				Email:    &newEmail,
+			},
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().
+					UpdateUser(gomock.Any(), gomock.Any()).
+					Times(1).
+					Return(db.User{}, sql.ErrNoRows)
+			},
+			buildContext: func(t *testing.T, tokenMaker token.Maker) context.Context {
+				return newContextWithBearerToken(t, tokenMaker, newUsername, time.Minute)
+			},
+			checkResponse: func(t *testing.T, res *pb.UpdateUserResponse, err error) {
+				require.Error(t, err)
+				st, ok := status.FromError(err)
+				require.True(t, ok)
+				require.Equal(t, codes.NotFound, st.Code())
+			},
+		},
+	}
+
+	for i := range cases {
+		tc := cases[i]
+
+		t.Run(tc.name, func(t *testing.T) {
+			// gomock controller
+			storeCtrl := gomock.NewController(t)
+			defer storeCtrl.Finish()
+
+			// mock db store
+			store := mockdb.NewMockStore(storeCtrl)
+
+			// run case
+			tc.buildStubs(store)
+
+			// new http server
+			testServer := newTestServer(t, store, nil)
+
+			// 构建上下文
+			ctx := tc.buildContext(t, testServer.tokenMaker)
+
+			res, err := testServer.UpdateUser(ctx, tc.req)
+
+			// check response
+			tc.checkResponse(t, res, err)
+
+		})
+	}
+
+}
